95cb8b7618cca080d26f0b97d81b1a286d91d0bd
Maxime PRUNIER <maxime.prunier@itsf.io>
2025-01-14 16:38:55 +0100

    Fix test clean sql

--- src/test/resources/sql/clean.sql
+++ src/test/resources/sql/clean.sql
@@ -1,3 +1,4 @@
+BEGIN;
 TRUNCATE TABLE test_result_screenshot CASCADE;
 TRUNCATE TABLE test_result_video CASCADE;
 TRUNCATE TABLE test_result CASCADE;
@@ -10,6 +11,7 @@ TRUNCATE TABLE suite_configuration CASCADE;
 TRUNCATE TABLE file_configuration CASCADE;
 TRUNCATE TABLE synchronization_error CASCADE;
 TRUNCATE TABLE synchronization CASCADE;
+TRUNCATE TABLE scheduler_configuration CASCADE;
 TRUNCATE TABLE environment_variable CASCADE;
 TRUNCATE TABLE environment CASCADE;
-TRUNCATE TABLE scheduler_configuration CASCADE;
+COMMIT;
